5680c70ac1387c4c8e2977cbe7560b2ff479b847
Pedro <piter500monzon@gmail.com>
2026-09-19 02:02:26 -0600

    Corregir serializacion de Redis: usar JSON en vez del serializador nativo de Java
    RedisCache intentaba serializar ProductoResponse con JdkSerializationRedisSerializer
    (requiere Serializable) y fallaba con SerializationException al escribir en cache
    tras un cache-miss. Se configura GenericJackson2JsonRedisSerializer como
    serializador de valores en RedisCacheConfig, evitando depender de que cada DTO
    cacheado implemente Serializable.

--- src/main/java/com/proyecto/servicios/config/RedisCacheConfig.java
+++ src/main/java/com/proyecto/servicios/config/RedisCacheConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCust
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -26,7 +27,8 @@ public class RedisCacheConfig {
     public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
         RedisCacheConfiguration configuracion = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(cacheTtlHours))
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
 
         return builder -> builder.withCacheConfiguration("productos", configuracion);
     }
